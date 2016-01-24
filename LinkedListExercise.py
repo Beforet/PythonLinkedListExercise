fef4715c72b6f5f238b33c4382aa17c5fd1398fe
Patrick Ford <t.beforet@gmail.com>
2016-01-23 21:12:46 -0500

    Add dummy methods. Define Node class

---
+++ LinkedListExercise.py
@@ -0,0 +1,32 @@
+"""
+
+Implementation of a singly linked list using Python.
+
+"""
+
+class Node(object):
+    def __init__(self, data, next = None):
+        this.data = data
+        this.next = next
+        
+class LinkedList(object):
+    def __init__(self):
+        pass
+    
+    def append(self, data):
+        pass
+    
+    def prepend(self, data):
+        pass
+    
+    def deleteFirst(self):
+        pass
+    
+    def deleteLast(self):
+        pass
+        
+    def delete(self, position):
+        pass
+        
+    def add(self, position):
+        pass
